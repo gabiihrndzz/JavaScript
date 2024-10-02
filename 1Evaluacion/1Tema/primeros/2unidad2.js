e52ea3fb1590f7f5f335755f3bbd4e9e1e9b08a6
Gabriel <gabriel.hdezcorrea@gmail.com>
2024-10-02 11:46:56 +0200

    2/10

--- 1Evaluacion/1Tema/primeros/2unidad2.js
+++ 1Evaluacion/1Tema/primeros/2unidad2.js
@@ -1,3 +1,4 @@
+/*
 const libs = ["React","Vue","Angular"];
 document.write("<br/>")
 document.write(`Este curso trata de ${libs[0]}`);
@@ -14,3 +15,68 @@ document.write(`
     ${l>1 ? `Estas ${l} bibliotecas` : "Esta biblioteca"}:
     ${libs.join(", ")}
 `)
+*/
+function testUnidad2_1(){
+let nombre;
+let nota;
+nombre=prompt('introduzca su nombre: ','');
+nota=prompt('introduzca su nota: ','');
+if ( nota >=5){
+    alert(nombre + ' ha aprobado con un '+nota);
+
+}
+else{
+    alert(nombre + ' ha suspendido con un '+nota);
+}
+}
+
+//Backup
+/*let nota1, nota2, nota3;
+nota1=prompt('introduzca su primera nota: ','');
+nota2=prompt('introduzca su segunda nota: ','');
+nota3=prompt('introduzca su tercera nota: ','');
+nota1=parseInt(nota1);
+nota2=parseInt(nota2);
+nota3=parseInt(nota3);
+let global;
+global=(nota1+nota2+nota3)/3;
+document.write(global);
+if(global>=9){
+    alert('Sobresaliente');
+}
+else{
+    if(global>=7){
+        alert('notable');
+    }
+    else{
+        if(global>=5){
+            alert('aprobado');
+        }
+        else{
+            if(global<5){
+                alert('suspenso');
+            }
+}
+    }}
+
+*/
+function testUnidad2_1bck(){
+    let nota1, nota2, nota3;
+    nota1=prompt('introduzca su primera nota: ','');
+    nota2=prompt('introduzca su segunda nota: ','');
+    nota3=prompt('introduzca su tercera nota: ','');
+    nota1=parseInt(nota1);
+    nota2=parseInt(nota2);
+    nota3=parseInt(nota3);
+    let global;
+    global=(nota1+nota2+nota3)/3;
+    document.write('la media da: '+global);
+    if(global>=9){
+        alert('Sobresaliente');}
+    else if(global>=7){
+            alert('notable');}
+    else if(global>=5){
+            alert('aprobado');}
+    else{
+        alert('suspenso');}
+    }
